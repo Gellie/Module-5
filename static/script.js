d148dca6fa2238dc75b6a14b8f3909523def07a4
Gellie <quiatchongellie@ymail.com>
2015-09-01 00:15:46 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -12,12 +12,12 @@
 
         var list_element = $('<li>');
 
-        list_element.html(thesis.Year + ' ' + thesis.Title + ' - by ' + thesis.Thesis.created_by  + ' ' + '<br><a href=\"/delete/' + thesis.id + '\"><button id=\"delete\" type=\"submit\">Delete</button></a>' + ' ' + '<a href=\"/edit/' + thesis.id + '\"><button id=\"edit\" type=\"submit\">Edit</button></a><br>');
+        list_element.html(thesis.Year + ' ' + thesis.Title + ' - by ' + thesis.created_by + ' ' + '<a href=\"/edit/' + thesis.id + '\"><button id=\"edit\" type=\"submit\">Edit</button></a>' + ' ' + '<a href=\"/delete/' + thesis.id + '\"><button id=\"delete\" type=\"submit\">Delete</button></a>');
 
         var thesis_entry_api = '/api/thesis'
         $.post(thesis_entry_api, thesis, function(response){
             if (response.status = 'OK'){
-                var full_detail = response.data.Year + ' ' + response.data.Title + ' - by ' + response.data.Thesis.created_by + ' ' + '<br><a href=\"/delete/' + response.data.id + '\"><button id=\"delete\" type=\"submit\">Delete</button></a>' + ' ' + '<a href=\"/edit/' + response.data.id + '\"><button id=\"edit\" type=\"submit\">Edit</button></a><br>'
+                var full_detail = response.data.Year + ' ' + response.data.Title + ' - by ' + response.data.created_by + ' ' + '<a href=\"/edit/' + response.data.id + '\"><button id=\"edit\" type=\"submit\">Edit</button></a>' + ' ' + '<a href=\"/delete/' + response.data.id + '\"><button id=\"delete\" type=\"submit\">Delete</button></a>'
 
                 $('.thesis-list').prepend('<li>' + full_detail + '</li>');
                 $('.create-form').trigger("reset");
@@ -33,7 +33,7 @@
         $.get(thesis_entry_api, {}, function(response){
             console.log('Thesis list', response)
             response.data.forEach(function(thesis){
-                var full_detail = thesis.Year + ' ' + thesis.Title + ' - by ' + thesis.Thesis.created_by + ' ' + '<br><a href=\"/delete/' + thesis.id + '\"><button id=\"delete\" type=\"submit\">Delete</button></a>' + ' ' + '<a href=\"/edit/' + thesis.id + '\"><button id=\"edit\" type=\"submit\">Edit</button></a><br>';
+                var full_detail = thesis.Year + ' ' + thesis.Title + ' - by ' + thesis.created_by + ' ' + '<a href=\"/edit/' + thesis.id + '\"><button id=\"edit\" type=\"submit\">Edit</button></a>' + ' ' + ' ' + '<a href=\"/delete/' + thesis.id + '\"><button id=\"delete\" type=\"submit\">Delete</button></a>';
                 $('.thesis-list').append('<li>' +  full_detail + '</li>')
             });
         });
